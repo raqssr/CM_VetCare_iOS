f421be9c3ba78c88d5f64698a3e690aff3d81166
Raquel Ramos <raquelramos@Raquels-MBP.lan>
2018-04-26 12:20:08 +0100

    tabs added

---
+++ VetCare/VetCare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VetCare
+//
+//  Created by Raquel Ramos on 26/04/2018.
+//  Copyright © 2018 raquelramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
